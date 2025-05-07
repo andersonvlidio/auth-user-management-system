db6cd88ade61d81e4f5f401db453cd135e7a3df2
andersonvlidio <andersonvlidio@gmail.com>
2025-05-06 22:24:55 -0300

    feat: create private routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,15 +3,43 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import UserProfile from './pages/UserProfile';
+import Register from './pages/Register';
+import AuthenticateToken from './components/AuthenticateToken';
 
 const NavRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+        
+        <Route
+          path="/"
+          element={
+            <AuthenticateToken>
+              <Dashboard />
+            </AuthenticateToken>
+          }
+        />
+        
+        <Route
+          path="/userprofile/:id"
+          element={
+            <AuthenticateToken>
+              <UserProfile />
+            </AuthenticateToken>
+          }
+        />
+
+        <Route
+          path="/register"
+          element={
+            <AuthenticateToken>
+              <Register />
+            </AuthenticateToken>
+          }
+        />
+
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/userprofile/:id" element={<UserProfile />} />
       </Routes>
     </BrowserRouter>
   );
